d41f1d3b790619e6f60690238f091d4abf194d72
Luan <luan.tavares.lourenco@gmail.com>
2021-06-12 09:45:47 -0300

    Add V3 get tickets and deals

---
+++ src/Resources/DealV3.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DevHokage\HubspotAPI\Resources;
+
+use DevHokage\HubspotAPI\Core\Resource;
+
+class DealV3 extends Resource
+{
+    protected $resource = "deals-v3";
+}
---
+++ src/Resources/TicketV3.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DevHokage\HubspotAPI\Resources;
+
+use DevHokage\HubspotAPI\Core\Resource;
+
+class TicketV3 extends Resource
+{
+    protected $resource = "tickets-v3";
+}
